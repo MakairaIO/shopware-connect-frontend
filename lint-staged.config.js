98bebf3a64c95abefa8cc88877cf8d3d4c4072d9
BenjaminJoergerMakaira <benjamin.joerger@makaira.io>
2025-04-22 13:57:01 +0200

    adjsut linter

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   "*.md": ["prettier --write"],
   "*.xml": [
-    "prettier --write --plugin=@prettier/plugin-xml --print-width 120 --tab-width 2",
+    "prettier --write --plugin=@prettier/plugin-xml --print-width 200 --tab-width 2",
   ],
   "*.yml": ["prettier --write"],
   "*.js": ["prettier --write"],
   "*.json": ["prettier --write"],
   "*.php": [
     "vendor/bin/php-cs-fixer fix --quiet --config=.php-cs-fixer.dist.php src",
-    "git add",
   ],
 };
